ecfc3c84f79b8c9085b5820bdf852fcaea82de6a
Даниил <danya.kusaev@mail.ru>
2020-07-18 16:15:40 +0300

    last update

--- posts/views.py
+++ posts/views.py
@@ -1,11 +1,29 @@
-from django.shortcuts import render, get_object_or_404
+from datetime import datetime
 
+from django.shortcuts import render, get_object_or_404, redirect
+
+from .forms import NewPostForm
 from .models import Post, Group
 
 
 def index(request):
     latest = Post.objects.order_by("-pub_date")[:11]
     return render(request, "index.html", {"posts": latest})
+
+
+def new_post(request):
+    if request.method == "POST":
+        form = NewPostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.save()
+            return redirect("index")
+
+        return render(request, "new_post.html", {"form": form})
+
+    form = NewPostForm()
+    return render(request, "new_post.html", {"form": form})
 
 
 def group_posts(request, slug):
