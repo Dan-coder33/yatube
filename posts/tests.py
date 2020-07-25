2efc6e9ca1e87cb10ea8dc3fb3c93b9b4d619c10
Даниил <danya.kusaev@mail.ru>
2020-07-25 15:09:30 +0300

    last update

--- posts/tests.py
+++ posts/tests.py
@@ -1,10 +1,78 @@
-from django.test import TestCase
+from django.contrib.auth.models import User
+from django.shortcuts import get_object_or_404
+from django.test import TestCase, Client
+from django.urls import reverse
+
+from posts.models import Post
 
 
-class TestStringMethods(TestCase):
-    def test_length(self):
-        self.assertEqual(len('yatube'), 6)
+class TestPostsCreation(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.client_auth = Client()
+        self.user = User.objects.create_user(
+            username="sarah", email="connor.s@skynet.com", password="12345"
+        )
+        self.client_auth.force_login(self.user)
+        Post.objects.create(text="text", author=self.user)
 
-    def test_show_msg(self):
-        # действительно ли первый аргумент — True?
-        self.assertTrue(False, msg="Важная проверка на истинность")
+    def test_profile(self):
+        response = self.client.get(
+            reverse(
+                "profile",
+                kwargs=dict(username=self.user.username)
+            )
+        )
+        self.assertEqual(response.status_code, 200)
+
+    def test_new_post(self):
+        posts_number_before = Post.objects.all().count()
+        response = self.client_auth.post(
+            reverse("new_post"),
+            data={"text": "test"}
+        )
+        self.assertEqual(response.status_code, 302)
+        posts_number_after = Post.objects.all().count()
+        self.assertEqual(posts_number_before + 1, posts_number_after)
+        self.assertTrue(Post.objects.filter(text="test").exists())
+
+    def test_new_post_cancel(self):
+        posts_number_before = Post.objects.all().count()
+        self.client.post(reverse("new_post"), data={"text": "teeest"})
+        posts_number_after = Post.objects.all().count()
+        self.assertEqual(posts_number_before, posts_number_after)
+        self.assertFalse(Post.objects.filter(text="teeeest").exists())
+
+    def check_urls(self, post, post_text):
+        for url in (
+            reverse("index"),
+            reverse("profile", kwargs={"username": self.user.username}),
+            reverse("post", kwargs={
+                "username": self.user.username,
+                "post_id": post.id,
+            })
+        ):
+            response = self.client.get(url)
+            self.assertContains(response, post_text)
+
+    def test_post_pg(self):
+        post_text = "text"
+        post = Post.objects.create(text=post_text, author=self.user)
+        self.check_urls(post, post_text)
+
+    def test_post_pg_edit(self):
+        post_text = "edit_text"
+        post = get_object_or_404(Post, text="text")
+        post_id = post.id
+        self.client_auth.post(
+            reverse(
+                "post_edit",
+                kwargs={
+                    "username": self.user.username,
+                    "post_id": post_id,
+                }
+            ),
+            data={"text": post_text}
+        )
+
+        self.check_urls(post, post_text)
--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,5 @@
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
 from django.shortcuts import render, get_object_or_404, redirect
 from django.core.paginator import Paginator
 
@@ -6,20 +8,18 @@ from .models import Post, Group
 
 
 def index(request):
-    # latest = Post.objects.order_by("-pub_date")[:11]
-    # return render(request, "index.html", {"posts": latest})
-
-    post_list = Post.objects.order_by('-pub_date').all()
+    post_list = Post.objects.order_by("-pub_date").all()
     paginator = Paginator(post_list, 10)
-    page_number = request.GET.get('page')
+    page_number = request.GET.get("page")
     page = paginator.get_page(page_number)
     return render(
         request,
-        'index.html',
-        {'page': page, 'paginator': paginator}
+        "index.html",
+        {"page": page, "paginator": paginator}
     )
 
 
+@login_required
 def new_post(request):
     if request.method == "POST":
         form = NewPostForm(request.POST)
@@ -29,24 +29,75 @@ def new_post(request):
             post.save()
             return redirect("index")
 
-        return render(request, "new_post.html", {"form": form})
+        return render(request, "new_post.html", {"form": form, "edit": False})
 
     form = NewPostForm()
-    return render(request, "new_post.html", {"form": form})
+    return render(request, "new_post.html", {"form": form, "edit": False})
 
 
 def group_posts(request, slug):
     group = get_object_or_404(Group, slug=slug)
-
-    # posts = Post.objects.filter(group=group).order_by("-pub_date")[:12]
-    # return render(request, "group.html", {"group": group, "posts": posts})
-
     post_list = Post.objects.filter(group=group).order_by("-pub_date").all()
     paginator = Paginator(post_list, 10)
-    page_number = request.GET.get('page')
+    page_number = request.GET.get("page")
     page = paginator.get_page(page_number)
     return render(
         request,
-        'group.html',
-        {'page': page, 'paginator': paginator}
+        "group.html",
+        {"page": page, "paginator": paginator, "group": group}
+    )
+
+
+def profile(request, username):
+    user = get_object_or_404(User, username=username)
+    post_list = user.posts.order_by("-pub_date").all()
+    count = post_list.count()
+    paginator = Paginator(post_list, 10)
+    page_number = request.GET.get("page")
+    page = paginator.get_page(page_number)
+    return render(
+        request,
+        "profile.html",
+        {
+            "username": username,
+            "page": page,
+            "paginator": paginator,
+            "count": count,
+            "author": user,
+        }
+    )
+
+
+def post_view(request, username, post_id):
+    post = get_object_or_404(Post, author__username=username, id=post_id)
+    post_list = post.author.posts.all()
+    count = post_list.count()
+    return render(
+        request,
+        "post.html",
+        {
+            "username": username,
+            "post_id": post_id,
+            "post": post,
+            "count": count,
+            "author": post.author,
+        }
+    )
+
+
+def post_edit(request, username, post_id):
+    post = get_object_or_404(Post, author__username=username, id=post_id)
+
+    if post.author != request.user:
+        return redirect("post", username, post_id)
+
+    form = NewPostForm(data=request.POST or None, instance=post)
+    if form.is_valid():
+        post.save()
+        return redirect("post", username, post_id)
+
+    return render(
+        request,
+        "new_post.html",
+        {"form": form, "edit": True, "post": post}
     )
