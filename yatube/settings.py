4c3f1684fb5d0edfba79d8c8ffd23fbe8bca46fb
Даниил <danya.kusaev@mail.ru>
2020-08-01 20:22:12 +0300

    last update

--- yatube/settings.py
+++ yatube/settings.py
@@ -25,10 +25,7 @@ SECRET_KEY = 'ivbbq@34t386=6lh#c(zzls7m*t5md)9-loti!_e=c-ar&+!9b'
 DEBUG = True
 
 ALLOWED_HOSTS = [
-    "localhost",
-    "127.0.0.1",
-    "[::1]",
-    "testserver",
+    "*",
 ]
 
 # Application definition
@@ -45,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "debug_toolbar",
 ]
 
 MIDDLEWARE = [
@@ -55,6 +53,11 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "debug_toolbar.middleware.DebugToolbarMiddleware",
+]
+
+INTERNAL_IPS = [
+    "127.0.0.1",
 ]
 
 ROOT_URLCONF = 'yatube.urls'
