fe61b4894a5f6017cbd97dba4bcbc58f7f77b6a0
Даниил <danya.kusaev@mail.ru>
2020-12-16 14:50:07 +0300

    last update

--- yatube/settings.py
+++ yatube/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'ivbbq@34t386=6lh#c(zzls7m*t5md)9-loti!_e=c-ar&+!9b'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     "*",
